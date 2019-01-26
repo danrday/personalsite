cecc8bdd4865dab095c41b0d0a457fa568d9a73a
euf7315 <Daniel.Day@ParallonTechnology.com>
2019-01-26 10:45:47 -0600

    add tone js

--- frontend/pages/webaudio.js
+++ frontend/pages/webaudio.js
@@ -1,17 +1,25 @@
 import React, { Component } from 'react'
+import Tone from 'tone'
 
 class WebAudio extends Component {
 
     componentDidMount() {
 
         function onMIDIMessage (message) {
+            const synth = new Tone.Synth().toMaster();
+            synth.triggerAttackRelease("C4", "8n");
             console.log(message.data);
+        }
+
+        if (navigator.requestMIDIAccess) {
+            navigator.requestMIDIAccess()
+                .then(success, failure);
         }
 
         function success (midi) {
             console.log('Got midi!', midi);
-            var inputs = midi.inputs.values();
-            for (var input = inputs.next();
+            let inputs = midi.inputs.values();
+            for (let input = inputs.next();
                  input && !input.done;
                  input = inputs.next()) {
                 // each time there is a midi message call the onMIDIMessage function
@@ -23,10 +31,16 @@ class WebAudio extends Component {
             console.error('No access to your midi devices.')
         }
 
-        if (navigator.requestMIDIAccess) {
-            navigator.requestMIDIAccess()
-                .then(success, failure);
+        // listen to keyboard in case no midi device attached
+        document.onkeypress = function (e) {
+            function onKeyPress (key) {
+                const synth = new Tone.Synth().toMaster();
+                synth.triggerAttackRelease("C4", "50n");
+            }
+            e = e || window.event;
+            onKeyPress(e.key)
         }
+
     }
 
     render() {
